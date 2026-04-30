9ff4b2ef4608763a7ae0ce42f4a690dbafce8d81
Daniel Terreros <110783387+DanielTKC@users.noreply.github.com>
2026-04-30 12:35:59 -0500

    Cors implemmented in middleware

--- server.js
+++ server.js
@@ -1,8 +1,24 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const cors = require('cors');
 const PORT = process.env.PORT || 3500;
 
+
+// CORS
+const whitelist = ['http://localhost:3000', 'https://www.yourdomain.com'];
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  optionsSuccessStatus: 200
+}
+
+app.use(cors());
 
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
