115782fbfba31ba85513d7aceb34b011440285e9
Daniel Terreros <110783387+DanielTKC@users.noreply.github.com>
2026-04-30 12:24:34 -0500

    Adding urlencoded and json middleware

--- server.js
+++ server.js
@@ -4,7 +4,11 @@ const path = require('path');
 const PORT = process.env.PORT || 3500;
 
 
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
 app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.all('*path', (req, res) => {
   res.status(404).sendFile(path.join(__dirname, 'public', '404.html'));
