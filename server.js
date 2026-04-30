a13c6efd18ab79d03d99697183a839cc7f5b4ca9
Daniel Terreros <110783387+DanielTKC@users.noreply.github.com>
2026-04-30 12:06:53 -0500

    Landing page at root added

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3500;
 
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 })
 
 app.listen(PORT, () => (console.log(`Listening on port ${PORT}`)))
