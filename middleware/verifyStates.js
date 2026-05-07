60051221eb6363d3b896019e19a7685aa846c23f
Daniel Terreros <110783387+DanielTKC@users.noreply.github.com>
2026-05-07 17:54:53 -0500

    Fixed wrong message for invalid param

--- middleware/verifyStates.js
+++ middleware/verifyStates.js
@@ -4,7 +4,7 @@ const stateCodes = statesData.map(state => state.code);
 const verifyStates = async (req, res, next) => {
   const code = req.params.state?.toUpperCase();
   if (!stateCodes.includes(code)) {
-    return res.status(400).json({message: 'Invalid state Code'});
+    return res.status(400).json({message: 'Invalid state abbreviation parameter' });
   }
   req.code = code;
   next();
